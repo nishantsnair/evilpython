4600671d6937d79d129302e2dca187cf7ca31f96
Zach Siegel <zsiegel92@gmail.com>
2019-05-06 20:20:09 -0700

    edits

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def basic_response():
-	print("Someone clicked this link!")
-	return "This is a Website!"
+    print("Someone clicked this link!")
+    return "This is a Website!"
 
 @app.route("/click/<user>")
 def name_response(user):
-	print("{} clicked this link!".format(user))
-	return "This is a Website!"
+    print("{} clicked this link!".format(user))
+    return "This is a Website!"
 
-app.run(host='0.0.0.0')
+app.run()
